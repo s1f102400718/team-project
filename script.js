53f90c4979482b5ea153c3e2006ec5353cc8dd94
s1f102400718 <s1f102400718@iniad.org>
2024-06-28 16:51:24 +0900

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 Result Skip Results Iframe
 EDIT ON
-  $('.slider').slick({
+$('.slider').slick({
     arrows: false,//左右の矢印はなし
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
@@ -12,17 +12,17 @@ EDIT ON
     slidesToShow: 4,//スライドを画面に4枚見せる
     slidesToScroll: 1,//1回のスライドで動かす要素数
     responsive: [
-      {
-      breakpoint: 769,//モニターの横幅が769px以下の見せ方
-      settings: {
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
         slidesToShow: 2,//スライドを画面に2枚見せる
-      }
+    }
     },
     {
-      breakpoint: 426,//モニターの横幅が426px以下の見せ方
-      settings: {
-        slidesToShow: 1.5,//スライドを画面に1.5枚見せる
-      }
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+        slidesToShow: 1,//スライドを画面に1.5枚見せる
     }
-  ]
-  });
+    }
+]
+});
