41ebb5d1fb55c74f5167c7de470d5fd4d177bc06
s1f102400718 <s1f102400718@iniad.org>
2024-06-28 15:49:06 +0900

    スライドしよー

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const slideshow = document.querySelector('.slideshow');
-    const slides = Array.from(slideshow.children);
-    const slideWidth = slides[0].getBoundingClientRect().width;
-
-    slides.forEach((slide, index) => {
-        slide.style.left = `${slideWidth * index}px`;
-    });
-
-    function spanDuration() {
-        return slides.length * 1;
+Result Skip Results Iframe
+EDIT ON
+  $('.slider').slick({
+    arrows: false,//左右の矢印はなし
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+    speed: 6900,//スライドのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+    pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+    cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+    slidesToShow: 4,//スライドを画面に4枚見せる
+    slidesToScroll: 1,//1回のスライドで動かす要素数
+    responsive: [
+      {
+      breakpoint: 769,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+      }
+    },
+    {
+      breakpoint: 426,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+      }
     }
-
-    slideshow.style.animationDuration = `${spanDuration()}s`;
-
-    setInterval(() => {
-        slideshow.appendChild(slideshow.firstElementChild);
-        const currentTransformValue = getComputedStyle(slideshow).transform;
-        slideshow.style.transition = 'none';
-        slideshow.style.transform = 'none';
-        void slideshow.offsetWidth;
-        slideshow.style.transition = '';
-        slideshow.style.transform = currentTransformValue;
-    }, spanDuration() * 1000);
-});
+  ]
+  });
